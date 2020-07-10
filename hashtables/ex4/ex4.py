827c30d0352b686d3b5f8c8d6e6b1e6d1eff232f
Michael Walter <michael@MacBook-Pro.local>
2020-07-10 10:52:23 -0700

    ex4.py passing

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -4,6 +4,25 @@ def has_negatives(a):
     """
     # Your code here
 
+    # Create a cache
+    cache = {}
+
+    # Size the cache and add the keys, set values to None
+    for i in a :
+        if i not in cache:
+            cache[i] = None
+
+    # Create a final list to be returned
+    result = []
+
+    # Iterate through the cache
+    # If the key is positive and 
+    # has a matching negative(true value)
+    # then add the value 
+    for true_num in cache.items():
+        if true_num[0] > 0 and -true_num[0] in cache:
+            result.append(true_num[0])
+
     return result
 
 
