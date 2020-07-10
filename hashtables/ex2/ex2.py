249c2460436a00ad0b4e69e7bcea4ec20728f518
Michael Walter <michael@MacBook-Pro.local>
2020-07-10 09:26:00 -0700

    ex2.py passing

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -11,4 +11,30 @@ def reconstruct_trip(tickets, length):
     """
     # Your code here
 
-    return route
+    # Create a cache
+    routes = {}
+
+    # Iterate through the list of tickets, adding routes into # the cache
+    for route in tickets:
+        if route not in routes:
+            routes[route.source] = route.destination
+
+    # Starting route will be 'NONE'
+    starting_souce = routes['NONE']
+
+    # The final list including the starting, to be returned in the end
+    stops = [starting_souce]
+
+    # Iterate through the tickets list
+    for i in range(length-1):
+        next_dest = stops[i]
+
+        # The next stop should be the value of
+        # the current source
+        next_stop = routes[next_dest]
+
+        # Add our next stop to the list of stops
+        stops.append(next_stop)
+
+
+    return stops
